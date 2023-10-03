7b8a242859e02eef47e9e86819e328b49bcd0874
zombiehunter <viktormallory25@gmail.com>
2023-10-03 10:59:52 +0300

    task36 solved

---
+++ task36/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный
+// случайными числами. Найдите сумму элементов, стоящих
+// на нечётных позициях
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] CreateRandIntArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+
+    return arr;
+}
+
+string Join(int[] array, string sep = " ", string wrapLeft = "", string wrapRight = "")
+{
+    string output = "";
+    for (int i = 0; i < array.Length; i++)
+    {
+        output += (i < array.Length - 1) ? $"{array[i]}{sep}" : $"{array[i]}";
+    }
+
+    return $"{wrapLeft}{output}{wrapRight}";
+}
+
+int OddNumPositionSum(int[] numbers)
+{
+    int sum = 0;
+    for (int i = 1; i < numbers.Length; i += 2)
+    {
+        sum += numbers[i];
+    }
+
+    return sum;
+}
+
+int[] randArr = CreateRandIntArray(4, -10, 11);
+int result = OddNumPositionSum(randArr);
+Console.Write(Join(randArr, ", ", "[", "]"));
+Console.WriteLine($" -> {result}");
+
