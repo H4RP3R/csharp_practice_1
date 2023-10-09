5cd7cc5dbce27cdbc616c2dc2e52426dd2f00b63
zombiehunter <viktormallory25@gmail.com>
2023-10-09 23:53:30 +0300

    task50 solved

---
+++ task50/Program.cs
@@ -0,0 +1,96 @@
+﻿// Задача 50: Напишите программу, которая на вход
+// принимает позиции элемента в двумерном массиве, и
+// возвращает значение этого элемента или же указание,
+// что такого элемента нет.
+
+
+using System;
+
+public class Answer
+{
+    public static void PrintArray(int[,] matrix)
+    {
+        // Введите свое решение ниже
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write($"{matrix[i, j]}\t");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public static int[,] CreateIncreasingMatrix(int n, int m, int k)
+    {
+        // Введите свое решение ниже
+        int[,] matrix = new int[n, m];
+        int value = 1;
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrix[i, j] = value;
+                value += k;
+            }
+        }
+
+        return matrix;
+    }
+
+    public static int[] FindNumberByPosition(int[,] matrix, int rowPosition, int columnPosition)
+    {
+        // Введите свое решение ниже
+        rowPosition--;
+        columnPosition--;
+        if (rowPosition < matrix.GetLength(0) && columnPosition < matrix.GetLength(1))
+        {
+            return new int[2] { matrix[rowPosition, columnPosition], 0 };
+        }
+        return new int[2];
+    }
+
+    public static void PrintCheckIfError(int[] results, int X, int Y)
+    {
+        // Введите свое решение ниже
+        if (results[0] == 0 && results[1] == 0)
+        {
+            Console.WriteLine("There is no such index");
+        }
+        else
+        {
+            Console.WriteLine($"The number in [{X}, {Y}] is {results[0]}");
+        }
+    }
+
+
+    // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args)
+    {
+        int n, m, k, x, y;
+
+        if (args.Length >= 5)
+        {
+            n = int.Parse(args[0]);
+            m = int.Parse(args[1]);
+            k = int.Parse(args[2]);
+            x = int.Parse(args[3]);
+            y = int.Parse(args[4]);
+        }
+        else
+        {
+            // Здесь вы можете поменять значения для отправки кода на Выполнение
+            n = 3;
+            m = 4;
+            k = 2;
+            x = 8;
+            y = 3;
+        }
+
+        // Не удаляйте строки ниже
+        int[,] result = CreateIncreasingMatrix(n, m, k);
+        PrintArray(result);
+        PrintCheckIfError(FindNumberByPosition(result, x, y), x, y);
+    }
+}
