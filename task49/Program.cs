4edbde54c524bfc31a90da1c75fb3128d6a2462e
zombiehunter <viktormallory25@gmail.com>
2023-10-09 22:19:04 +0300

    task49 solved

---
+++ task49/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у
+// которых оба индекса чётные, и замените эти элементы на их
+// квадраты.
+
+int[,] GenerateRandomMatrix(int rows, int cols, int min, int max)
+{
+    int[,] matrix = new int[rows, cols];
+    Random rand = new();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(min, max);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    string divider = string.Empty;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        divider += "------";
+    }
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4} |");
+        }
+        Console.WriteLine();
+        Console.WriteLine(divider);
+    }
+}
+
+void ReplaceEvenIndxNumbers(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i += 2)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j += 2)
+        {
+            matrix[i, j] *= matrix[i, j];
+        }
+    }
+}
+
+int[,] rndMtrx = GenerateRandomMatrix(6, 5, 0, 9);
+PrintMatrix(rndMtrx);
+Console.WriteLine();
+ReplaceEvenIndxNumbers(rndMtrx);
+PrintMatrix(rndMtrx);
